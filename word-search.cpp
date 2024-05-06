a9264c8d6d02c7e6e417b30d3eac05af99d544ab
Jay <jaykapadia389@gmail.com>
2024-05-06 22:35:26 +0530

    word search code accepted

--- word-search.cpp
+++ word-search.cpp
@@ -1,67 +1,46 @@
 class Solution {
 public:
-    bool isValid(map<pair<int, int>, bool> mp, int i, int j, int pi, int pj,
-                 int m, int n) {
 
-        if (!mp[{i, j}] && (i < n && i >= 0) && (j < m && j >= 0) &&
-            !(i == pi && j == pj)) {
+    int dc[4] = {1,-1,0,0} ;
+    int dr[4] = {0,0,-1,1} ;
+    bool isValid( int i, int j, int m, int n) {
+
+        if ((i < n && i >= 0) && (j < m && j >= 0)) {
             return true;
         }
         return false;
     }
 
     bool recc(map<pair<int, int>, bool>& mp, vector<vector<char>>& board, int n,
-              int m, int i, int j, string word, int e, int pi, int pj) {
-        cout << i << " " << j << endl ;
-        
+              int m, int i, int j, string word, int e) {
+
+        if(mp[{i,j}]){
+            return false ;
+        }
+
         if (board[i][j] != word[e]) {
             return false;
         }
 
-
         if (e + 1 == word.length()) {
-            cout << "end " << i << j ; 
             return true;
         }
 
-        if (isValid(mp, i - 1, j, pi, pj, m, n)) {
+        mp[{i,j}] = 1 ;
 
-            // map 1
-            mp[{i - 1, j}] = 1;
-            if (recc(mp, board, n, m, i - 1, j, word, e + 1, i, j)) {
-                return true;
+        for(int x = 0 ; x < 4 ; x++){
+
+            if (isValid(i + dc[x], j + dr[x], m, n)) {
+
+                if (recc(mp, board, n, m,i + dc[x], j + dr[x], word, e + 1)) {
+                    return true;
+                }
+
             }
 
-            // map 0
-            mp[{i - 1, j}] = 0;
         }
 
-        if (isValid(mp, i + 1, j, pi, pj, m, n)) {
-            mp[{i + 1, j}] = 1;
-
-            if (recc(mp, board, n, m, i + 1, j, word, e + 1, i, j)) {
-                return true;
-            }
-            mp[{i + 1, j}] = 0;
-        }
-
-        if (isValid(mp, i, j - 1, pi, pj, m, n)) {
-            mp[{i, j - 1}] = 0;
-
-            if (recc(mp, board, n, m, i, j - 1, word, e + 1, i, j)) {
-                return true;
-            }
-            mp[{i, j - 1}] = 1;
-        }
-
-        if (isValid(mp, i, j + 1, pi, pj, m, n)) {
-            mp[{i, j + 1}] = 0;
-
-            if (recc(mp, board, n, m, i, j + 1, word, e + 1, i, j)) {
-                return true;
-            }
-            mp[{i, j + 1}] = 1;
-        }
+        mp[{i,j}] =0 ;
 
         return false;
     }
@@ -73,7 +52,7 @@ public:
         map<pair<int, int>, bool> mp;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (recc(mp, board, n, m, i, j, word, 0, -1, -1)) {
+                if (recc(mp, board, n, m, i, j, word, 0)) {
                     return true;
                 }
             }
