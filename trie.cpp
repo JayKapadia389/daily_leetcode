6764fe4004c7f8618d6f0eb2857c0e7f27409ae2
Jay <jaykapadia389@gmail.com>
2024-07-27 18:14:33 +0530

    trie-insertion and searching

--- trie.cpp
+++ trie.cpp
@@ -4,7 +4,7 @@ using namespace std ;
 
 class Node{
 
-    public :
+  public :
 
   Node* children[26];
   bool endOfWord ;  
@@ -26,11 +26,65 @@ class Trie{
     Trie(){
         root = new Node() ;
     }
+
+    void insert(string str){
+
+      Node * temp = root ;
+      int idx ;
+
+      for(int i = 0 ; i < str.length() ; i++){
+
+        idx = str[i] - 'a';
+
+        if((temp->children)[idx] == NULL){
+          (temp->children)[idx] = new Node() ;
+        }
+
+        if(i == (str.length() - 1)){
+          (temp->children)[idx]->endOfWord = true ;
+        }
+
+        temp = (temp->children)[idx] ;
+
+      }
+
+    }
+
+    bool exist(string str){
+
+      Node * temp = root ;
+      int idx ;
+
+      for(int i = 0 ; i < str.length() ; i++){
+
+        idx = str[i] - 'a' ;
+
+        if((temp->children)[idx] == NULL){
+          return false ;
+        }
+
+        if(i == (str.length() - 1)){
+          if((temp->children)[idx]->endOfWord) {
+            return true ;
+          }
+          else{
+            return false ;
+          }
+        }
+
+        temp = (temp->children)[idx] ;
+
+      }
+
+    }
 };
 
 int main(){
 
-Trie * myTrie = new Trie() ;
+Trie myTrie ;
+
+myTrie.insert("the") ;
+cout << myTrie.exist("the") ;
 
 }
 
