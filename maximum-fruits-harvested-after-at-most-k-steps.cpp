25d4870349641ecc8d1759c5fb21f59077258905
Jay <jaykapadia389@gmail.com>
2025-08-03 22:13:22 +0530

    maximum fruits harvested after at most k steps__daily question__hard

---
+++ maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int left = 0;
+        int right = 0;
+        int n = fruits.size();
+        int sum = 0;
+        int ans = 0;
+
+        auto step = [&](int left, int right) -> int {
+            if (fruits[right][0] <= startPos) {
+                return startPos - fruits[left][0];
+            } else if (fruits[left][0] >= startPos) {
+                return fruits[right][0] - startPos;
+            } else {
+                return min(abs(startPos - fruits[right][0]),
+                           abs(startPos - fruits[left][0])) +
+                       fruits[right][0] - fruits[left][0];
+            }
+        };
+        // each time fix the right boundary of the window
+        while (right < n) {
+            sum += fruits[right][1];
+            // move left boundary
+            while (left <= right && step(left, right) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+            ans = max(ans, sum);
+            right++;
+        }
+        return ans;
+    }
+};
