4b83aa9cb127db5bf1510e9327120ff9cd0c281f
Jay <jaykapadia389@gmail.com>
2025-12-07 23:22:03 +0530

    count odd numbers in an interval range__daily question__easy

---
+++ count-odd-numbers-in-an-interval-range.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int len = high - low + 1;
+        if(len%2 == 0){
+            return len/2;
+        }
+        else{
+            if(low%2 == 0){
+                return len/2;
+            }
+            else{
+                return len/2 + 1;
+            }
+        }
+    }
+};
