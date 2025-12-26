bb9695ef47c069795e0d074ae789b44952317708
Jay <jaykapadia389@gmail.com>
2025-12-26 14:59:09 +0530

    minimum penalty for a shop__daily question__medium

---
+++ minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int minPenalty = 0, curPenalty = 0;
+        int earliestHour = 0;
+
+        for (int i = 0; i < customers.size(); i++) {
+            char ch = customers[i];
+
+            if (ch == 'Y') {
+                curPenalty--;
+            } else {
+                curPenalty++;
+            }
+
+            if (curPenalty < minPenalty) {
+                earliestHour = i + 1;
+                minPenalty = curPenalty;
+            }
+        }
+
+        return earliestHour;
+    }
+};
