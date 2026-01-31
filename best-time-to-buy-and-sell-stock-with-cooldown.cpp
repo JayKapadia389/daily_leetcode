4ed92d14d8c12c3f7eb653bdb28794428797f5a1
Jay <jaykapadia389@gmail.com>
2026-01-31 15:06:51 +0530

    find-smallest-letter-greater-than-target__daily question__easy

--- best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,3 +1,5 @@
+// Solution 1:
+// Top-down DP (memo)
 class Solution {
 private:
     vector<vector<int>> dp;
@@ -31,5 +33,27 @@ public:
     int maxProfit(vector<int>& prices) {
         dp = vector<vector<int>>(prices.size(), vector<int>(2, -1));
         return recc(prices, prices.size(), 0, false);
+    }
+};
+
+// Solution 2:
+// bottom-up DP with space optimization.
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> prev1(2, 0);
+        vector<int> prev2(2, 0);
+        vector<int> curr(2, 0);
+
+        for(int i = n-1; i >= 0; --i){
+            curr[0] = max(prices[i] + prev2[1], 0 + prev1[0]);
+            curr[1] = max(-prices[i] + prev1[0], 0 + prev1[1]);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr[1];
     }
 };
