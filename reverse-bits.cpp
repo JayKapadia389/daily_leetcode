8bbe9d7c36ad776ec439f597b626803f64f6855b
Jay <jaykapadia389@gmail.com>
2026-02-16 21:21:29 +0530

    reverse-bits__daily question__easy

---
+++ reverse-bits.cpp
@@ -0,0 +1,16 @@
+// Time - O(1) (32 shifts)
+// Space - O(1) (32 bit integer)
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans = 0;
+
+        for(int i = 0; i < 32; ++i){
+            ans <<= 1;
+            ans += (n & 1);
+            n >>= 1;
+        }
+
+        return ans;
+    }
+};
